f87298993472c29b45e20c9d0a9eeadb2d5ab46b
qiliang.huang <goosehuangmatt@gmail.com>
2024-10-11 17:58:22 +0800

    Add HeapBinaryTree class for creating a binary tree with heap property
    - Added a new file heap_binary_tree.py
    - Implemented the Node class with methods for adding nodes randomly and maintaining the heap property
    - Implemented the HeapBinaryTree class with methods for adding values and popping the head and bottom nodes
    - Added a method for parsing the tree by layers

--- heap_binary_tree.py
+++ heap_binary_tree.py
@@ -30,11 +30,51 @@ class Node:
                 self.right.random_add_node(node)
 
 
+    def heap_add_node(self, node):
+        if type(node)!= Node:
+            node = Node(node)
+
+        if node.val>=self.val:
+
+            if self.left and self.right:
+                node.left = self.pop_left()
+                node.right = self.pop_right()
+
+                if random.randint(0,1):
+                    res = node.left.heap_add_node(self)
+                    node.left = res
+                else:
+                    res = node.right.heap_add_node(self)
+                    node.right = res            
+
+            elif self.left:
+                node.left = self.pop_left()
+                node.right = self
+            elif self.right:
+                node.right = self.pop_right()
+                node.left = self
+            else:
+                node.random_add_node(self)
+
+            return node
+        
+        else:
+            if self.left and self.right:
+                if random.randint(0,1):
+                    res = self.left.heap_add_node(node)
+                    self.left = res
+                else:
+                    res = self.right.heap_add_node(node)
+                    self.right = res      
+            else:
+                self.random_add_node(node)
+
+            return self
+
     def pop_left(self):
         node = self.left
         self.left = None
         return node
-
 
     def pop_right(self):
         node = self.right
@@ -43,18 +83,20 @@ class Node:
 
 
 
-class BinaryTree:
+class HeapBinaryTree:
     def __init__(self):
         self.head = None
 
-    def random_add_node(self, node):
+    def add_value(self, node):
         if type(node)!= Node:
             node = Node(node)
 
         if not self.head:
             self.head = node
         else:
-            self.head.random_add_node(node)
+            res = self.head.heap_add_node(node)
+            self.head = res
+
 
     def pop_bottom(self): # for pop_head()
         cur = self.head
@@ -143,36 +185,33 @@ class BinaryTree:
             else:
                 break
         
+        total_num = 0
         for i in range(max_layer):
             cur_layer_list = layer_dict[i]
             print(i, [node.val for node in cur_layer_list])
+            total_num += len(cur_layer_list)
             # print(i, len([node.val for node in cur_layer_list]))
+        print("#:", total_num)
 
         return layer_dict
     
 
 
 def main():
-    # node_list = [1, 2, 86, 18, 5, 9, 20, 10, 21, 236, 0, 7, 904, 1006, 709, 57, 357, 48, 100, 478, 2093, 1238]
-    N = 100
-    node_list = [int(round(random.randint(0, 10*N)/N, 0)) for _ in range(N)]
-    print(node_list)
+    node_list = [1, 2, 86, 18, 5, 9, 20, 10, 21, 236, 0, 7, 904, 1006, 709, 57, 357, 48, 100, 478, 2093, 1238]
+    # N = 100
+    # node_list = [int(round(random.randint(0, 10*N)/N, 0)) for _ in range(N)]
 
-    tree = BinaryTree()
+    print("#:",len(node_list))
+    # print(node_list)
+
+    tree = HeapBinaryTree()
     for node in node_list:
-        tree.random_add_node(node)
+        tree.add_value(node)
 
     tree.parse_by_layer()
 
-    # res = tree.pop_bottom()
-    # print("bottom:", res.val)
 
-    res = tree.pop_head()
-    print("head:", res.val)
-
-
-    print("new head:", tree.head.val)
-    tree.parse_by_layer()
 
 
     return
